aa5535854ec1adf25aaf4f0e541f08a23d526443
ufotablee <ufotablee@gmail.com>
2024-05-01 14:53:57 +0300

    213

--- index.js
+++ index.js
@@ -1,3 +1,78 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const selectButtonLang = document.querySelector(".select__lang-selected");
+  const selectListLang = document.querySelector(".select-lang__list");
+  const mobileMenu = document.querySelector(".mobile__menu");
+  const btnHumburger = document.querySelector(".header__humburger");
+  const btnCloseMobile = document.querySelector(".head__menu-close");
+  const mobileMenuInner = document.querySelector(".mobile__menu-inner");
+
+  if (btnHumburger && mobileMenu && btnCloseMobile) {
+    btnHumburger.addEventListener("click", function () {
+      toggleMobileMenu(true);
+    });
+
+    btnCloseMobile.addEventListener("click", function () {
+      toggleMobileMenu(false);
+    });
+
+    document.addEventListener("mouseup", function (e) {
+      if (
+        mobileMenu.classList.contains("show-mobile") &&
+        !mobileMenuInner.contains(e.target)
+      ) {
+        toggleMobileMenu(false);
+      }
+    });
+  }
+
+  function toggleMobileMenu(show) {
+    mobileMenu.classList.toggle("show-mobile", show);
+    document.body.style.overflow = show ? "hidden" : "auto";
+  }
+
+  if (selectButtonLang && selectListLang) {
+    selectButtonLang.addEventListener("click", function () {
+      selectListLang.classList.toggle("show");
+      this.classList.toggle("has_show");
+    });
+    document.addEventListener("click", function (event) {
+      const isClickInsideList = selectListLang.contains(event.target);
+      const isClickOnButton = selectButtonLang.contains(event.target);
+
+      if (!isClickInsideList && !isClickOnButton) {
+        selectListLang.classList.remove("show");
+        selectButtonLang.classList.remove("has_show");
+      }
+    });
+  }
+});
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth <= 600) {
+    window.removeEventListener("scroll", scrollHandler);
+  } else {
+    window.addEventListener("scroll", scrollHandler);
+  }
+});
+
+function scrollHandler() {
+  const header = document.querySelector("header");
+  const main = document.querySelector("main");
+  const scrollY = window.scrollY || window.pageYOffset;
+
+  if (scrollY > 45) {
+    header.classList.add("fixed");
+    main.classList.add("change");
+  } else {
+    header.classList.remove("fixed");
+    main.classList.remove("change");
+  }
+}
+
+if (window.innerWidth > 600) {
+  window.addEventListener("scroll", scrollHandler);
+}
+
 let swiperPrevBtn = document.querySelector(".swiper-button-prev");
 let swiperNextBtn = document.querySelector(".swiper-button-next");
 
@@ -5,10 +80,10 @@ let swiperNextBtn = document.querySelector(".swiper-button-next");
 const swiper = new Swiper(".swiper1", {
   // Optional parameters
   direction: "horizontal",
-  loop: true,
-  // autoplay: {
-  //   delay: 5000,
-  // },
+  speed: 1200,
+  autoplay: {
+    delay: 3000,
+  },
   // If we need pagination
   pagination: {
     el: ".swiper-pagination",
@@ -30,6 +105,18 @@ const swiper = new Swiper(".swiper1", {
     el: ".swiper-scrollbar",
   },
 });
+swiper.on("slideChange", function () {
+  if (swiper.isBeginning) {
+    swiperPrevBtn.classList.add("disabled");
+  } else {
+    swiperPrevBtn.classList.remove("disabled");
+  }
+  if (swiper.isEnd) {
+    swiperNextBtn.classList.add("disabled");
+  } else {
+    swiperNextBtn.classList.remove("disabled");
+  }
+});
 
 const swiper2 = new Swiper(".swiper2", {
   direction: "horizontal",
@@ -37,7 +124,10 @@ const swiper2 = new Swiper(".swiper2", {
   // autoplay: {
   //   delay: 5000,
   // },
-
+  effect: "fade",
+  fadeEffect: {
+    crossFade: true,
+  },
   pagination: {
     el: ".swiper-pagination",
     type: "progressbar",
@@ -53,6 +143,7 @@ const swiper2 = new Swiper(".swiper2", {
     prevEl: ".swiper-button-prev",
   },
 
+  allowTouchMove: false,
   // And if we need scrollbar
   scrollbar: {
     el: ".swiper-scrollbar",
